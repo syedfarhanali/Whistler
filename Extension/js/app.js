896465be9e3058ccd80a39a4f665c3b08ebe30b8
syedfarhanali <farhans@Fidelis03.local>
2014-09-15 12:19:08 +0530

    UI changes and link preview initial changes

--- Extension/js/app.js
+++ Extension/js/app.js
@@ -4,7 +4,71 @@ $(document).ready(function(){
 		var options = { direction:'right'};
 		var duration = 500;
 		$('#slide_container').toggle(effect, options, duration);
+			
+		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+		    var curUrl = tabs[0].url;
+		    $(".whistle_link").children('textarea').html(curUrl);
+		});
 	});
+	/*
+	 * 
+	 */
+	$('textarea').liveUrl({
+		  success : function(data) 
+		  {  
+		    console.log(data);
+		    var preview =data.video+'<br/>'+data.title+'<br/>'+data.description;
+		    $("#linkPreview").html(preview);
+		    // this return the first found url data
+		  }
+		});
+	
+	//$("#linkUrl").urlive({container: '#linkPreview'});
+	
+	/*$('textarea').on('input propertychange', function () {
+	     $('textarea').urlive({
+	        callbacks: {
+	            onStart: function () {
+	                $('.loading').show();
+	                $('.urlive-container').urlive('remove');
+	            },
+	            onSuccess: function (data) {
+	                $('.loading').hide();
+	                $('.urlive-container').urlive('remove');
+	            },
+	            noData: function () {
+	                $('.loading').hide();
+	            }
+	        }
+	    });
+	}).trigger('input');*/
+	
+	
+	$('#linkUrl').on('input propertychange', function () {
+		console.log(1);
+	     $('#linkUrl').urlive({
+	        callbacks: {
+	            onStart: function () {
+	            	console.log(2);
+	                $('.loading').show();
+	                $('#linkPreview').urlive('remove');
+	            },
+	            onSuccess: function (data) {
+	            	console.log(3);
+	                $('.loading').hide();
+	                $('#linkPreview').urlive('remove');
+	            },
+	            noData: function () {
+	            	console.log(4);
+	                $('.loading').hide();
+	            }
+	        }
+	    });
+	}).trigger('input');
+	
+	
+	
+	
 	
 	$("#close_slider").unbind('click').click(function(){
 		$("#new_post").click();
@@ -17,7 +81,6 @@ $(document).ready(function(){
 	
 	$(".tag_container").click(function(){
 		var obj = $(this).children("[name=tag_image]");
-		console.log(obj);
 		if(obj.hasClass("whistle_tag")){
 			obj.removeClass("whistle_tag");
 			obj.addClass("whistle_tag_active");
@@ -25,6 +88,16 @@ $(document).ready(function(){
 			obj.addClass("whistle_tag");
 			obj.removeClass("whistle_tag_active");
 		}
+	});
+	
+	$(".list_item").on('click',".favourite",function(){
+		console.log("12");
+		$(this).removeClass('favourite').addClass('favourite_sel');
+	})
+	
+	$(".list_item").on('click',".favourite_sel",function(){
+		console.log("121111");
+		$(this).removeClass('favourite_sel').addClass('favourite');
 	})
 	
 })
