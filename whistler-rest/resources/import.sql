aad72bb32b919f312d33ba3e1def2e89c784f757
Gurpreet Singh <gurpreets@fidelis-inc.com>
2014-09-17 21:21:47 +0530

    migration for entities

--- whistler-rest/resources/import.sql
+++ whistler-rest/resources/import.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS user;
 CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
@@ -6,3 +7,30 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS user_group_join;
+CREATE TABLE `user_group_join` (
+  `user_id` int(11) unsigned NOT NULL,
+  `group_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS group;
+CREATE TABLE `group` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS whistle;
+CREATE TABLE `whistle` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `image_url` tinytext,
+  `url` text,
+  `up_vote` int(11) DEFAULT NULL,
+  `down_vote` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+
--- whistler-rest/src/main/java/com/talentica/whistler/controller/WhistleController.java
+++ whistler-rest/src/main/java/com/talentica/whistler/controller/WhistleController.java
@@ -1,17 +1,13 @@
 package com.talentica.whistler.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/whistler")
 public class WhistleController {
 
-	private final AtomicLong counter = new AtomicLong();
-
-	@RequestMapping("/whistler")
 	public String greeting(@RequestParam(value="name", required=false, defaultValue="Whistler") String name) {
 		return "Hello Whistler";
 	}
