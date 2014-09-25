4764c1badcb1b3517f35826d5fdecb7a9ed20e51
syedfarhanali <farhans@Fidelis03.local>
2014-09-25 11:26:06 +0530

    adding Clan dao

---
+++ whistler-rest/src/main/java/com/talentica/whistler/dao/ClanDaoImpl.java
@@ -0,0 +1,28 @@
+package com.talentica.whistler.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.talentica.whistler.bo.QueryBo;
+import com.talentica.whistler.common.SQLQueryIds;
+import com.talentica.whistler.common.Util;
+import com.talentica.whistler.common.WConstants;
+import com.talentica.whistler.entity.Clan;
+
+@Repository
+public class ClanDaoImpl extends BaseDaoImpl<Clan> implements ClanDao{
+
+	@Autowired
+	private QueryBo queryBo;
+	
+	public ClanDaoImpl() {
+		super(Clan.class);
+	}
+
+	public List<Clan> findMyClans(Integer userId){
+		javax.persistence.Query query = entityManager.createNativeQuery(queryBo.getQueryString(SQLQueryIds.FIND_MY_CLANS,new Object[]{userId}));
+		return Util.getClansFromList(query.getResultList());
+	}
+}
